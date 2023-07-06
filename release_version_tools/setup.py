b6d82f4969ff7b9a7f4a66cca8921a9920cc940a
semantic-release <semantic-release>
2023-07-06 15:36:16 -0500

    0.2.0
    Automatically generated by python-semantic-release

--- release_version_tools/setup.py
+++ release_version_tools/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-__version__ = "0.0.0"
+__version__ = "0.2.0"
 
 setup(
    name="setup",
    version=__version__,
     packages=(
         find_packages() +
-        find_packages(where="./")
+        find_packages(where=".c/")
     ),
 )
