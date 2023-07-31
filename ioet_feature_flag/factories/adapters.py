3de1641a41156bda7df224367c27a963e1c2bac1
Mikkel-14 <jose.munoz01@epn.edu.ec>
2023-07-31 16:44:42 +0000

    feat: FF-5 returned to JSON adapter

--- ioet_feature_flag/factories/adapters.py
+++ ioet_feature_flag/factories/adapters.py
@@ -1,6 +1,6 @@
 from ..adapters.base import FeatureRepositoryAdapter
-from ..adapters.aws_appconfig_adapter import AWSAppConfigAdapter
+from ..adapters.json_adapter import JSONAdapter
 
 
 def get_toggle_configuration() -> FeatureRepositoryAdapter:
-    return AWSAppConfigAdapter()
+    return JSONAdapter(config_path="feaure_toggles.json")
