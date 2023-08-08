c61fe2c0d592244e973c5b4db8c1f569e219a17b
Mikkel-14 <jose.munoz01@epn.edu.ec>
2023-08-08 21:38:07 +0000

    feat: FF-37 make Json provider environment aware

--- ioet_feature_flag/providers/json_toggle_provider.py
+++ ioet_feature_flag/providers/json_toggle_provider.py
@@ -1,20 +1,27 @@
 import json
 import typing
+import os
 
-from ..exceptions import ToggleNotFoundError
+from ..exceptions import ToggleNotFoundError, ToggleEnvironmentError
 from .provider import Provider
 
 
 class JsonToggleProvider(Provider):
     def __init__(self, toggles_file_path: str) -> None:
         self.path = toggles_file_path
+        self._environment = os.getenv("ENVIRONMENT")
+        if not self._environment:
+            raise ToggleEnvironmentError(
+                "Could not retrieve toggles: Toggle environment not specified"
+            )
 
     def get_toggles(self, toggle_names: typing.List[str]) -> typing.Tuple[bool, ...]:
         with open(self.path, "r") as toggles_file:
             toggles = json.load(toggles_file)
+            environment_toggles = toggles[self._environment]
 
             missing_toogles = [
-                toggle for toggle in toggle_names if toggle not in toggles
+                toggle for toggle in toggle_names if toggle not in environment_toggles
             ]
             if missing_toogles:
                 raise ToggleNotFoundError(
@@ -23,6 +30,6 @@ class JsonToggleProvider(Provider):
 
             return tuple(
                 bool(toggle_value)
-                for toggle_name, toggle_value in toggles.items()
+                for toggle_name, toggle_value in environment_toggles.items()
                 if toggle_name in toggle_names
             )
--- tests/providers/json_toggle_provider_unit_test.py
+++ tests/providers/json_toggle_provider_unit_test.py
@@ -4,7 +4,7 @@ import os
 import typing
 
 from ioet_feature_flag.providers import JsonToggleProvider, Provider
-from ioet_feature_flag.exceptions import ToggleNotFoundError
+from ioet_feature_flag.exceptions import ToggleNotFoundError, ToggleEnvironmentError
 
 _TOGGLES_FILE = "/tmp/app_toggles_test.json"
 
@@ -18,35 +18,70 @@ def _clear_toggles_file():
 
 @pytest.fixture(name="add_toggles")
 def _add_toggles():
-    def _add(toggles: typing.Dict[str, bool]) -> None:
+    def _add(toggles: typing.Dict[str, typing.Dict[str, bool]]) -> None:
         with open(_TOGGLES_FILE, "w") as toggles_file:
             json.dump(toggles, toggles_file)
+
     return _add
 
 
+@pytest.fixture(autouse=True)
+def _del_environment(monkeypatch):
+    yield
+    if os.getenv("ENVIRONMENT"):
+        monkeypatch.delenv("ENVIRONMENT")
+
+
 class TestGetTogglesMethod:
-    def test__returns_the_toggles_specified_in_the_file(
+    @pytest.mark.parametrize("environment_name", [("production"), ("stage")])
+    def test__returns_the_toggles_specified_in_the_file_when_called_on_environment(
         self,
-        add_toggles: typing.Callable[[typing.Dict[str, bool]], None],
+        environment_name: str,
+        monkeypatch,
+        add_toggles: typing.Callable[[typing.Dict[str, typing.Dict[str, bool]]], None],
     ):
-        toggles = {"some_toggle": True, "another_toggle": False}
+        monkeypatch.setenv("ENVIRONMENT", environment_name)
+        toggles = {environment_name: {"some_toggle": True, "another_toggle": False}}
         add_toggles(toggles)
 
         toggle_provider: Provider = JsonToggleProvider(_TOGGLES_FILE)
-        some_toggle, another_toggle = toggle_provider.get_toggles(["some_toggle", "another_toggle"])
+        some_toggle, another_toggle = toggle_provider.get_toggles(
+            ["some_toggle", "another_toggle"]
+        )
 
-        assert some_toggle == toggles["some_toggle"]
-        assert another_toggle == toggles["another_toggle"]
+        assert some_toggle == toggles[environment_name]["some_toggle"]
+        assert another_toggle == toggles[environment_name]["another_toggle"]
 
+    @pytest.mark.parametrize("environment_name", [("production"), ("stage")])
     def test__raises_an_error__if_one_of_the_toggles_does_not_exist(
         self,
         add_toggles: typing.Callable[[typing.Dict[str, bool]], None],
+        environment_name: str,
+        monkeypatch,
     ):
-        toggles = {"some_toggle": True}
+        monkeypatch.setenv("ENVIRONMENT", environment_name)
+        toggles = {environment_name: {"some_toggle": True}}
         add_toggles(toggles)
         toggle_provider: Provider = JsonToggleProvider(_TOGGLES_FILE)
 
         with pytest.raises(ToggleNotFoundError) as error:
             toggle_provider.get_toggles(["some_toggle", "another_toggle"])
 
-        assert str(error.value) == 'The follwing toggles where not found: another_toggle'
+        assert (
+            str(error.value) == "The follwing toggles where not found: another_toggle"
+        )
+
+    def test__raises_an_error_when_environment_not_specified(
+        self,
+        add_toggles: typing.Callable[[typing.Dict[str, bool]], None],
+    ):
+        toggles = {"undefined_env": {"some_toggle": True}}
+        add_toggles(toggles)
+
+        with pytest.raises(ToggleEnvironmentError) as error:
+            toggle_provider: Provider = JsonToggleProvider(_TOGGLES_FILE)
+
+        assert (
+            str(error.value)
+            == "Could not retrieve toggles: Toggle environment not specified"
+        )
