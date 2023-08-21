f87e1f92e598b35c9f6a78c306092f32f531f1c5
Franco Colmenarez <franco.colmenarez@ioet.com>
2023-08-21 17:23:06 -0500

    Revert toggle changes

--- ioet_feature_flag/toggles.py
+++ ioet_feature_flag/toggles.py
@@ -3,18 +3,12 @@ import typing
 from functools import wraps
 
 from . import exceptions, types
-from .providers import Provider, JsonToggleProvider
+from .providers import Provider
 
 
 class Toggles:
-    def __init__(
-        self,
-        provider: typing.Optional[Provider] = None,
-    ) -> None:
-        if provider:
-            self.provider = provider
-            return
-        self.provider = JsonToggleProvider('./toggles/toggles.json')
+    def __init__(self, provider: Provider) -> None:
+        self.provider = provider
 
     def toggle_decision(self, decision_function: types.TOOGLE_DECISION):
         @wraps(decision_function)
