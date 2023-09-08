91fe2256994a04f8e50285ef89e6a0a35fc5112b
Mikkel-14 <jose.munoz01@epn.edu.ec>
2023-09-08 14:14:58 +0000

    fix: FF-56 added current branch variable in shell script

--- scripts/test_import/test-import.sh
+++ scripts/test_import/test-import.sh
@@ -2,9 +2,10 @@
 set -eo pipefail
 
 function test_import() {
+    CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
     cd ../
     poetry new test-project && cd test-project
-    poetry add git+https://git@github.com/ioet/ioet-feature-flag.git
+    poetry add git+https://git@github.com/ioet/ioet-feature-flag.git@$CURRENT_BRANCH
     poetry add pytest
     export ENVIRONMENT="test"
     cp -r ../ioet-feature-flag/scripts/test_import/* ./
