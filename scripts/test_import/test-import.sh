577d02623a09e017a48c678ea8d8272c6b5027f4
Mikkel-14 <jose.munoz01@epn.edu.ec>
2023-09-27 14:59:47 +0000

    test: FF-65 Added library import test using pip

--- scripts/test_import/test-import.sh
+++ scripts/test_import/test-import.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
-function test_import() {
+function test_import_poetry() {
     CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
     cd ../
-    poetry new test-project && cd test-project
+    poetry new test-project-poetry && cd test-project-poetry
     poetry add git+https://git@github.com/ioet/ioet-feature-flag.git@$CURRENT_BRANCH
     poetry add pytest
     export ENVIRONMENT="test"
@@ -14,4 +14,18 @@ function test_import() {
 
 }
 
-test_import
+function test_import_pip() {
+    CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+    cd ../
+    mkdir test-project-pip && cd test-project-pip
+    python -m venv env && source env/bin/activate
+    pip install git+https://github.com/ioet/ioet-feature-flag.git@$CURRENT_BRANCH
+    pip install -U pytest
+    export ENVIRONMENT="test"
+    cp -r ../ioet-feature-flag/scripts/test_import/* ./
+    pytest
+    cd ../ioet-feature-flag
+}
+
+test_import_poetry
+test_import_pip
