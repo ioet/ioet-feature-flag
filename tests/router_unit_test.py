cd60e05fee18d9770e4ed722e68c16c0180d13c1
Miguel Muñoz <67567150+Mikkel-14@users.noreply.github.com>
2023-10-12 20:59:51 -0500

    refactor: FF-62 refactored unit tests to use mocks (#39)
    * refactor: FF-62 refactored unit tests to use mocks
    
    * fix: Autogenerated commit to ensure that there are at least two commits on all branches/PRs

--- tests/router_unit_test.py
+++ tests/router_unit_test.py
@@ -1,78 +1,55 @@
-import yaml
 import pytest
-import os
 import typing
 from freezegun import freeze_time
 
-from ioet_feature_flag.providers import YamlToggleProvider
 from ioet_feature_flag.exceptions import ToggleNotFoundError
 from ioet_feature_flag.router import Router
 from ioet_feature_flag.toggle_context import ToggleContext
 
-_TOGGLES_FILE = "/tmp/app_toggles_test.yaml"
-
-
-@pytest.fixture(autouse=True)
-def _clear_toggles_file():
-    yield
-    if os.path.exists(_TOGGLES_FILE):
-        os.remove(_TOGGLES_FILE)
-
-
-@pytest.fixture(name="add_toggles")
-def _add_toggles():
-    def _add(toggles: typing.Dict[str, typing.Dict[str, bool]]) -> None:
-        with open(_TOGGLES_FILE, "w") as toggles_file:
-            yaml.dump(toggles, toggles_file)
-
-    return _add
-
-
-@pytest.fixture(autouse=True)
-def _del_environment(monkeypatch):
-    yield
-    if os.getenv("ENVIRONMENT"):
-        monkeypatch.delenv("ENVIRONMENT")
-
 
 class TestGetTogglesMethod:
-    @pytest.mark.parametrize("environment_name", [("production"), ("stage")])
+    @pytest.fixture(name="dependency_factory")
+    def __dependency_factory(self, mocker) -> typing.Callable:
+        def _factory(toggle_values: typing.Dict):
+            toggle_names = toggle_values.keys()
+            return {
+                "provider": mocker.Mock(
+                    get_toggle_list=mocker.Mock(return_value=list(toggle_names)),
+                    get_toggle_attributes=mocker.Mock(
+                        side_effect=[
+                            toggle_values[toggle_name] for toggle_name in toggle_names
+                        ]
+                    ),
+                )
+            }
+
+        return _factory
+
     def test__returns_the_toggles_specified_in_the_file_when_called_on_environment(
         self,
-        environment_name: str,
-        monkeypatch,
-        add_toggles: typing.Callable[[typing.Dict[str, typing.Dict[str, bool]]], None],
+        dependency_factory: typing.Callable,
     ):
-        monkeypatch.setenv("ENVIRONMENT", environment_name)
         toggles = {
-            environment_name: {
-                "some_toggle": {"enabled": True},
-                "another_toggle": {"enabled": False},
-            }
+            "some_toggle": {"enabled": True},
+            "another_toggle": {"enabled": False},
         }
-        add_toggles(toggles)
 
-        toggle_provider = YamlToggleProvider(_TOGGLES_FILE)
-        toggle_router = Router(toggle_provider)
+        dependencies = dependency_factory(toggle_values=toggles)
+        toggle_router = Router(**dependencies)
         some_toggle, another_toggle = toggle_router.get_toggles(
             ["some_toggle", "another_toggle"]
         )
 
-        assert some_toggle == toggles[environment_name]["some_toggle"]["enabled"]
-        assert another_toggle == toggles[environment_name]["another_toggle"]["enabled"]
+        assert some_toggle == toggles["some_toggle"]["enabled"]
+        assert another_toggle == toggles["another_toggle"]["enabled"]
 
-    @pytest.mark.parametrize("environment_name", [("production"), ("stage")])
     def test__raises_an_error__if_one_of_the_toggles_does_not_exist(
         self,
-        add_toggles: typing.Callable[[typing.Dict[str, bool]], None],
-        environment_name: str,
-        monkeypatch,
+        dependency_factory: typing.Callable,
     ):
-        monkeypatch.setenv("ENVIRONMENT", environment_name)
-        toggles = {environment_name: {"some_toggle": {"enabled": True}}}
-        add_toggles(toggles)
-        toggle_provider = YamlToggleProvider(_TOGGLES_FILE)
-        toggle_router = Router(toggle_provider)
+        toggles = {"some_toggle": {"enabled": True}}
+        dependencies = dependency_factory(toggle_values=toggles)
+        toggle_router = Router(**dependencies)
 
         with pytest.raises(ToggleNotFoundError) as error:
             toggle_router.get_toggles(["some_toggle", "another_toggle"])
@@ -81,25 +58,20 @@ class TestGetTogglesMethod:
             str(error.value) == "The follwing toggles where not found: another_toggle"
         )
 
-    @pytest.mark.parametrize("environment_name", [("production"), ("stage")])
     def test__uses_the_toggle_context_when_it_is_provided(
         self,
         mocker,
-        monkeypatch,
-        add_toggles: typing.Callable[[typing.Dict[str, bool]], None],
-        environment_name: str,
+        dependency_factory: typing.Callable,
     ):
-        monkeypatch.setenv("ENVIRONMENT", environment_name)
-        toggles = {environment_name: {"some_toggle": {"enabled": True}}}
-        add_toggles(toggles)
+        toggles = {"some_toggle": {"enabled": True}}
         toggle_strategy = mocker.Mock(is_enabled=mocker.Mock(return_value=True))
         toggle_context = mocker.Mock()
         mocker.patch(
             "ioet_feature_flag.router.get_toggle_strategy",
             return_value=toggle_strategy,
         )
-        toggle_provider = YamlToggleProvider(_TOGGLES_FILE)
-        toggle_router = Router(toggle_provider)
+        dependencies = dependency_factory(toggle_values=toggles)
+        toggle_router = Router(**dependencies)
 
         toggle_router.get_toggles(["some_toggle"], toggle_context)
 
