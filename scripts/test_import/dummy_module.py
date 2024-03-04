5104dcdabe36e3beacd2c774ce1f943d741a4d51
Franco <franco.colmenarez@ioet.com>
2024-03-04 11:36:35 -0500

    test: FF-88 Add object oriented example with a dummy class (#58)
    * test: FF-88 Add object oriented example with dummy class
    
    * fix: Autogenerated commit to ensure that there are at least two commits on all branches/PRs
    
    ---------
    
    Co-authored-by: ioet ops <no-reply+github-actions@ioet.com>

--- scripts/test_import/dummy_module.py
+++ scripts/test_import/dummy_module.py
@@ -1,5 +1,7 @@
 import ioet_feature_flag
 
+import typing
+
 provider = ioet_feature_flag.YamlToggleProvider('./feature_toggles/feature-toggles.yaml')
 toggles = ioet_feature_flag.Toggles(provider)
 
@@ -51,3 +53,33 @@ def get_name_user():
             role="",
         ),
     )
+
+
+class DummyClass:
+    def __init__(self, feature_toggles: ioet_feature_flag.Toggles):
+        self._toggles = feature_toggles
+
+    @staticmethod
+    def _usage(
+        get_toggles: typing.Callable,
+        when_on,
+        when_off,
+        context: typing.Optional[ioet_feature_flag.ToggleContext] = None,
+    ):
+        is_new_greetings_message_enabled = get_toggles(
+            ["is_new_greetings_message_enabled"],
+            context
+        )
+        if is_new_greetings_message_enabled:
+            return when_on
+        return when_off
+
+    def run(self) -> str:
+        use_greetings_message = self._toggles.toggle_decision(self._usage)
+
+        cancellation_text = use_greetings_message(
+            when_on="Hello",
+            when_off="Hi",
+        )
+
+        return cancellation_text
--- scripts/test_import/dummy_test.py
+++ scripts/test_import/dummy_test.py
@@ -1,5 +1,7 @@
 import dummy_module
 
+import ioet_feature_flag
+
 
 def test_get_name():
     assert dummy_module.get_name() == "name-a"
@@ -11,3 +13,12 @@ def test_get_name_b():
 
 def test_get_name_user():
     assert dummy_module.get_name_user() == "name-a"
+
+
+def test_object_oriented():
+    provider = ioet_feature_flag.YamlToggleProvider(
+        './feature_toggles/feature-toggles.yaml'
+    )
+    toggles = ioet_feature_flag.Toggles(provider)
+    dummy_object = dummy_module.DummyClass(toggles)
+    assert dummy_object.run() == "Hello"
